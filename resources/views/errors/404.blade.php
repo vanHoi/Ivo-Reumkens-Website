83cd081827bbb8f01c49d066ee5f7c6b9fa24f51
Ivo Reumkens <ivoreum@gmail.com>
2019-01-12 00:21:15 +0100

    Updated 404 message

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,9 +8,12 @@
 		<div class="section no-pad-bot">
 			<div class="container">
 				<br><br>
-				<h1 class="header center orange-text text-darken-3">Foutmelding - 404</h1>
+				<h1 class="header center orange-text text-darken-3">Fout - 404</h1>
 				<div class="row center">
-					<h5 class="header col s11 text-darken-4">Er is iets mis gegaan! We konden deze pagina niet vinden.</h5>
+					<h5 class="header col s11 orange-text text-darken-4">Er is iets mis gegaan! We konden deze pagina niet vinden.</h5>
+				</div>
+				<div class="row center">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/t3otBjVZzT0?rel=0" frameborder="0" allowfullscreen></iframe>
 				</div>
 				<div class="row center">
 					<a href="{{url('/')}}" class="btn-large waves-effect waves-light orange lighten-1">Terug naar home</a>
@@ -20,18 +23,6 @@
 			</div>
 		</div>
 		<div class="parallax"><img src="{{url('/images/background1.jpg')}}" alt="Background Image"></div>
-	</div>
-	
-	<div class="container">
-		<div class="section center">
-			
-			<hr><br>
-			
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/t3otBjVZzT0?rel=0" frameborder="0" allowfullscreen></iframe>
-			
-			<br><br><hr>
-		
-		</div>
 	</div>
 
 @stop
