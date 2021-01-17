c8ae9b88b80a7174e841f0fc0ba0c4292a85f318
Ivo Reumkens <mail@ivoreumkens.nl>
2021-01-17 23:39:58 +0100

    Updated Timeline Builder and Ubuntu Fan Urls

--- resources/lang/nl/projects.php
+++ resources/lang/nl/projects.php
@@ -1,37 +1,37 @@
 <?php
 
 return [
-	
+
 	'no_link' => 'Geen link beschikbaar.',
 	'only_dutch' => 'Deze pagina is alleen beschikbaar in het Nederlands.',
 	'description'  => 'Beschrijving',
-	
+
 	'attent_title' => 'Rijschool Attent',
 	'attent_url' => 'https://attentrijschool.nl',
 	'attent_description' => 'Dit is de website van het bedrijf van mijn vader. Ik heb de website ervoor gemaakt. Aan de achterkant draait OctoberCMS, een content management systeem (ook wel CMS genoemd, zoals Wordpress) wat ervoor zorgt dat alle pagina\'s makkelijk aan te passen zijn.',
 	'attent_description_short' => 'Dit is de website van het bedrijf van mijn vader. Ik heb de website ervoor gemaakt. Aan de achterkant draait OctoberCMS, wat ervoor zorgt dat alle pagina\'s makkelijk aan te passen zijn.',
-	
+
 	'ubuntufan_title' => 'Ubuntu Fan',
-	'ubuntufan_url' => 'https://ubuntufan.org',
+	'ubuntufan_url' => 'https://ubuntufan.nl',
 	'ubuntufan_description' => 'Een website over het besturingssysteem Ubuntu, speciaal gericht op beginners. Deze website beheer ik al sinds mijn vijftiende, al was het toen nog gemaakt met een online website bouw tool. Ik ben begonnen met de site toen Ubuntu 10.04 net was uitgekomen, Inmiddels zijn we acht jaar later en heeft de website heeft inmiddels meer dan 10.000 bezoekers gehad.<br/>
 Deze versie van de website is gebouwd in PHP, met behulp van het Laravel framework.',
 	'ubuntufan_description_short' => 'Een website over het besturingssysteem Ubuntu, speciaal gericht op beginners. Deze website beheer ik al sinds mijn vijftiende, en heeft inmiddels meer dan 10.000 bezoekers gehad.',
-	
+
 	'timeline_title' => 'Tijdlijn Generator',
-	'timeline_url' => 'https://www.maakjetijdlijn.nl/',
+	'timeline_url' => 'https://ivoreumkens.nl/subsites/timeline/index.html',
 	'timeline_description' => 'Dit is een simpele tijdlijn generator. Voer een aantal gebeurtenissen en jaartallen in, en er wordt een tijdlijn van gemaakt!',
 	'timeline_description_short' => 'Dit is een simpele tijdlijn generator. Voer een aantal gebeurtenissen en jaartallen in, en er wordt een tijdlijn van gemaakt!',
-	
+
 	'dionscape_title' => 'DionScape',
 	'dionscape_url' => 'https://attentrijschool.nl/dionscape',
 	'dionscape_description' => 'DionScape is een spel gebouwd door mij een twee medeleerlingen. We hebben dit gemaakt in het zesde jaar van de middelbare school voor ons profielwerkstuk. Het is een game gemaakt voor de Oculus Rift, toentertijd DK2. Later heb ik zelf het spel voor Windows wat geüpdatet en uitgebreid. Waarschuwing: Er zitten veel inside-jokes in van onze vriendengroep.',
 	'dionscape_description_short' => 'DionScape is een spel gebouwd door mij een twee medeleerlingen. We hebben dit gemaakt in het zesde jaar van de middelbare school voor ons profielwerkstuk. Het is een game gemaakt voor de Oculus Rift.',
-	
+
 	'sudoku_title' => 'Sudoku Generator',
 	'sudoku_url' => '',
 	'sudoku_description' => 'Op de middelbare school heb ik samen met een andere leerling een sudoku programma geschreven in Visual Basic. In dit spel zitten onder andere drie verschillende niveaus, niveaus en muziek.',
 	'sudoku_description_short' => 'Op de middelbare school heb ik samen met een andere leerling een sudoku programma geschreven in Visual Basic. In dit spel zitten onder andere drie verschillende niveaus, achievements en muziek.',
-	
+
 	'famouspeopleriddle_title' => 'Famous People Raadsel',
 	'famouspeopleriddle_url' => 'http://members.home.nl/ivode2de/CKV/',
 	'famouspeopleriddle_description' => 'Op de middelbare school heb ik met een aantal medeleerlingen een soort interactieve puzzel gemaakt. Het is een quiz / zoek spel, met inclusief verhaal. De implementatie heb ik volledig gedaan. Mocht u de quiz willen uitvoeren, begin dan met het zoeken van de Stonehenge en klik erop.',
